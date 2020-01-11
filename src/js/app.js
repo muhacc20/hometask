decb609a1db70f8ac49f0d7f946bdc6570e2d3d9
Student <student@localhosts>
2020-01-11 21:24:22 +0500

    master

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+const REGULAR_CATEGORY = 'regular';
+const INCREASED_CATEGORY = 'increased';
+const SPECIAL_CATEGORY = 'special';
+
+const purchases = [
+    {
+        amound = 1000,
+        category: REGULAR_CATEGORY,
+    }
+    {
+        amound = 10000,
+        category:INCREASED_CATEGORY,
+         
+    } {
+        amound = 1000,
+        category:REGULAR_CATEGORY,
+    }  {
+category:SPECIAL_CATEGORY,
+    },
+];
+let cashback = 0
+
+
+
+
+
